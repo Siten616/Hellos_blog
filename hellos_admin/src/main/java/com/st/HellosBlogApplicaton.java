9244aa890b248eb50f67fdedb4c3003d8d91c862
Siten616 <shentao0616@qq.com>
2020-11-04 17:36:15 +0800

    初始化

---
+++ hellos_admin/src/main/java/com/st/HellosBlogApplicaton.java
@@ -0,0 +1,13 @@
+package com.st;
+
+/**
+ * Hello world!
+ *
+ */
+public class HellosBlogApplicaton
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ hellos_admin/src/test/java/com/st/HellosBlogApplicatonTest.java
@@ -0,0 +1,20 @@
+package com.st;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class HellosBlogApplicatonTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
